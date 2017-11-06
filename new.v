7dd7d50588091fbe69b294f3ff04ea57eae12a18
Ryutaro Yamaguchi <OCdt.Yamaguchi@gmail.com>
2017-11-06 14:23:48 +0900

    instructionMemory

---
+++ new.v
@@ -0,0 +1,23 @@
+// ------------ fig 4.6 ------------ 
+// fetch
+
+// if there was no branch instruction, this adder would work.
+module adderForPc(
+    input pc,
+    output nextPc
+);
+    assign nextPc = pc + 1;
+endmodule
+
+module instructionMemory(
+    input  programCounter,
+    output instruction
+);
+    reg [31:0] instructionMemory [0:255];
+
+    initial begin
+        $readmemb("sample.bnr", instructionMemory); // load binary file to instructionMemory
+    end
+
+    assign instruction = instructionMemory[programCounter];
+endmodule
