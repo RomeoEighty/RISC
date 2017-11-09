c49a74924781342b65b7a3bd0c02f87ba4465b89
Ryutaro Yamaguchi <OCdt.Yamaguchi@gmail.com>
2017-11-09 16:22:07 +0900

    register monitor

--- newResistorFile.v
+++ newResistorFile.v
@@ -3,20 +3,41 @@
 // input bit wide: 5
 //      2**5 = 32
 //      equal to input resistor address bit wide
-module registorFile(
-    input  [4:0]  readResitorAddress1,
-    input  [4:0]  readResitorAddress2,
-    input  [4:0]  writeResistorAddress,
-    input  [31:0] writeResistorData,
-    input  clock,
+module registerFile(
+    input  clk,
+    input  rstd,
+    input  [4:0]  readRegisterAddress1,
+    input  [4:0]  readRegisterAddress2,
+    input  [4:0]  writeRegisterAddress,
+    input  [31:0] writeRegisterData,
     input  writeEnable, // assert(1): enable, deassert(0): disable
     output [31:0] readData1,
     output [31:0] readData2
 );
-    reg [31:0] registor [0:31];
+    reg [31:0] register [0:31];
     
-    assign readData1 = registor[readResitorAddress1];
-    assign readData2 = registor[readResitorAddress2];
-    always @(posedge clock)
-        if (writeEnable == 1) registor[writeResistorAddress] <= writeResistorData;
+    assign readData1 = register[readRegisterAddress1];
+    assign readData2 = register[readRegisterAddress2];
+    always @(negedge rstd or posedge clk)
+        if (rstd == 0) register[0] <= 32'h00000000;
+        else if (writeEnable == 1) register[writeRegisterAddress] <= writeRegisterData;
+
+    initial begin
+        $monitor("#", $stime, "\n[00]%h [01]%h [02]%h [03]%h\n[04]%h [05]%h [06]%h [07]%h\n[08]%h [09]%h [10]%h [11]%h\n[12]%h [13]%h [14]%h [15]%h\n[16]%h [17]%h [18]%h [19]%h\n[20]%h [21]%h [22]%h [23]%h\n[24]%h [25]%h [26]%h [27]%h\n[28]%h [29]%h [30]%h [31]%h",register[0], register[1], register[2], register[3], register[4], register[5], register[6], register[7], register[8], register[9], register[10], register[11], register[12], register[13], register[14], register[15], register[16], register[17], register[18], register[19], register[20], register[21], register[22], register[23], register[24], register[25], register[26], register[27], register[28], register[29], register[30], register[31]);
+     end
+endmodule
+
+module writeback(
+    input clk,
+    input rstd,
+    input [31:0] nextpc,
+    output [31:0] pc
+);
+    reg [31:0] pc;
+
+    always @(negedge rstd or posedge clk)
+        begin
+            if(rstd == 0) pc <= 32'h00000000;
+            else if (clk == 1) pc <= nextpc;
+        end
 endmodule
--- writeback.v
+++ writeback.v
@@ -26,4 +26,8 @@ module reg_file(
     always @(negedge rstd or posedge clk)
         if (rstd == 0) rf [0] <= 32'h00000000;
         else if (wren == 0) rf [wa] <= wr;
+
+    initial begin
+        $monitor("#", $stime, "\n[00]%h [01]%h [02]%h [03]%h\n[04]%h [05]%h [06]%h [07]%h\n[08]%h [09]%h [10]%h [11]%h\n[12]%h [13]%h [14]%h [15]%h\n[16]%h [17]%h [18]%h [19]%h\n[20]%h [21]%h [22]%h [23]%h\n[24]%h [25]%h [26]%h [27]%h\n[28]%h [29]%h [30]%h [31]%h",rf[0], rf[1], rf[2], rf[3], rf[4], rf[5], rf[6], rf[7], rf[8], rf[9], rf[10], rf[11], rf[12], rf[13], rf[14], rf[15], rf[16], rf[17], rf[18], rf[19], rf[20], rf[21], rf[22], rf[23], rf[24], rf[25], rf[26], rf[27], rf[28], rf[29], rf[30], rf[31]);
+    end
 endmodule
